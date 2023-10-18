70d42081e3886e392d9d50bc44cff558bb527a78
Awacs <tomas.kosub@gmail.com>
2023-10-18 20:56:33 +0200

    first dex contract version

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -22,6 +22,35 @@ contract DEX {
     }
 
     function sell() external onlyOwner {
-        
+        uint allowance = associatedToken.allowance(owner, address(this));
+        require(allowance > 0, "There is no allowance set at this time. Make sure you set the allowance.");
+
+        bool sent = associatedToken.transferFrom(owner, address(this), allowance);
+        require(sent, "Transaction failed.");
+    }
+
+    function withdrawTokens() external onlyOwner {
+        uint balance = associatedToken.balanceOf(address(this));
+        associatedToken.transfer(owner, balance);
+    }
+
+    function withdrawFunds() external onlyOwner {
+        (bool sent, ) = payable(owner).call{ value: address(this).balance }("");
+        require(sent, "Transaction failed.");
+    }
+
+    function getPrice(uint numTokens) public view returns(uint) {
+        return numTokens * price;
+    }
+
+    function buy(uint numTokens) external payable {
+        require(getTokenBalance() >= numTokens, "Insufficient funds.");
+        require(getPrice(numTokens) == msg.value, "The funds provided do not match the price.");
+
+        associatedToken.transfer(msg.sender, numTokens);
+    }
+
+    function getTokenBalance() public view returns(uint) {
+        return associatedToken.balanceOf(address(this));
     }
 }
