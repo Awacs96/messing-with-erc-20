16600ba5ba062611f422d290d33000f19cbd5178
Tomas Kosub <tomas.kosub@gmail.com>
2023-10-18 13:12:36 +0200

    basic dex implementation

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,3 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DEX {
+    IERC20 public associatedToken;
+
+    uint price;
+    address owner;
+
+    constructor(IERC20 _token, uint _price) {
+        associatedToken = _token;
+        price = _price;
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not the owner.");
+        _;
+    }
+
+    function sell() external onlyOwner {
+        
+    }
+}
